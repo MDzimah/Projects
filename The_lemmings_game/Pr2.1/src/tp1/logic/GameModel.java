9458e1b72759417a8c51e2ba7c9736a0f24e22e6
RedMaker09 <manukwabe3452@gmail.com>
2024-11-03 12:35:48 +0100

    Pr1

--- The_lemmings_game/Pr2.1/src/tp1/logic/GameModel.java
+++
@@ -1,8 +0,0 @@
-package tp1.logic;
-
-public interface GameModel {
-	public abstract void update();
-	public abstract void exit();
-	public abstract boolean isFinished();
-	public abstract void reset();
-}
--- The_lemmings_game/Pr2.1/src/tp1/logic/GameWorld.java
+++
@@ -1,9 +0,0 @@
-package tp1.logic;
-
-public interface GameWorld {
-	public abstract boolean isInAir(Position pos);
-	public abstract boolean lemmingOnExit(Position pos);
-	public abstract void lemmingArrived();
-	public abstract void lemmingDead();
-	public abstract void add(GameObject go);
-}
